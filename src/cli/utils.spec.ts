d606559a3c6326bc2b096ed0bb456302e64aef70
Ana Marjanica <ana@eisberg-labs.com>
2020-06-29 22:45:20 +0200

    Add test

--- src/cli/utils.spec.ts
+++ src/cli/utils.spec.ts
@@ -1,6 +1,24 @@
 import test from 'ava';
-import { getVersion } from './utils';
+import {getRepository, getVersion} from './utils';
+import {GitRepository} from './models';
 
 test('return ngx-lib-starter version', t => {
   t.true(getVersion() != null, 'Resolves library version');
+});
+
+test('return git repository', t => {
+  t.deepEqual(getRepository({
+    projectName: 'ngx-project-name',
+    description: undefined,
+    owner: {
+      username: 'test123',
+      name: 'Test 123',
+      website: undefined,
+      email: 'test123@e'
+    },
+    gitRepositoryType: GitRepository.Github,
+    keywords: undefined,
+    license: undefined,
+    travis: undefined
+  }), 'https://github.com/test123/ngx-project-name');
 });
